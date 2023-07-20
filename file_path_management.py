eef383882a403e2577e893c6aafc4d64504b2c6c
Didi Hou <didi.hou@outlook.com>
2023-07-20 16:32:51 +0200

    /

--- file_path_management.py
+++ file_path_management.py
@@ -1,6 +1,9 @@
 # annoucing all file paths
 project_folder = '/Users/didihou/myProjects/didihou_master_project'
 
+seed_paper_urls = '/Users/didihou/myProjects/didihou_master_project/seed_paper_list.txt'
+connec_db_urls = '/Users/didihou/myProjects/didihou_master_project/connectome_database_paper_list.txt'
+
 gs_poten_urls = '/Users/didihou/myProjects/didihou_master_project/google_scholar_poten_urls.txt'
 wos_poten_urls = '/Users/didihou/myProjects/didihou_master_project/webofscience_poten_urls.csv'
 pubmed_pmc_poten_urls = '/Users/didihou/myProjects/didihou_master_project/pubmed_pmc_poten_urls.csv'
@@ -10,9 +13,8 @@ path_poten_csv = '/Users/didihou/myProjects/didihou_master_project/list_of_poten
 
 path_related_urls = '/Users/didihou/myProjects/didihou_master_project/list_of_related_literature.txt'
 path_related_csv = '/Users/didihou/myProjects/didihou_master_project/summary_table_of_related_studies.csv'
+
 pdf_folder_path = '/Users/didihou/myProjects/liter_pdfs'
-seed_paper_urls = '/Users/didihou/myProjects/didihou_master_project/seed_paper_list.txt'
-connec_db_urls = '/Users/didihou/myProjects/didihou_master_project/connectome_database_paper_list.txt'
 
 
 
