34bfd9f8112bf7560f8bbf958afd16b974bcaf02
Didi Hou <didi.hou@outlook.com>
2023-07-20 16:30:14 +0200

    /

--- public_library.py
+++ public_library.py
@@ -1,4 +1,7 @@
 # self-written public library
+
+
+# setting headers and proxies
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
 http_proxy  = "http://103.148.39.50:83"
 https_proxy = "https://47.254.158.115:20201"
@@ -6,6 +9,7 @@ proxy = {
     "http": http_proxy, 
     "https": https_proxy
 }
+# end of setting header and proxies
 
 def clear_file(file_path):
     with open(file_path, 'w') as f:
