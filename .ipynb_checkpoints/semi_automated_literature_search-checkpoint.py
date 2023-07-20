62e498374890d4cf605290ff40d23122c076ae44
Didi Hou <didi.hou@outlook.com>
2023-07-20 02:00:50 +0200

    /

--- .ipynb_checkpoints/semi_automated_literature_search-checkpoint.py
+++ .ipynb_checkpoints/semi_automated_literature_search-checkpoint.py
@@ -39,7 +39,7 @@ def search_google_scholar(init_url, headers):
         soup = BeautifulSoup(response.content,'lxml')
         # print(soup)
         # print(soup.select('[data-lid]')) 
-        for item in soup.select('[data-lid]'):
+        for item in soup.find_all('[data-lid]'):
             print(item)
             add_url = item.find_all('h3')[0].find_all('a', href = True)[0]['href']
             print(add_url)
