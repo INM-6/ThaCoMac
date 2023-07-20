df30494a434581d2e5ed7a280800a9dfcfdb10e0
Didi Hou <didi.hou@outlook.com>
2023-07-20 16:30:48 +0200

    /

--- public_library.py
+++ public_library.py
@@ -1,6 +1,5 @@
 # self-written public library
 
-
 # setting headers and proxies
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
 http_proxy  = "http://103.148.39.50:83"
