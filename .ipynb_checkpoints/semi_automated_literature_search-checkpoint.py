4a22920eec36f28940a9b6f4a1c8fb6dd67411b3
Didi Hou <didi.hou@outlook.com>
2023-07-20 02:22:19 +0200

    /

--- .ipynb_checkpoints/semi_automated_literature_search-checkpoint.py
+++ .ipynb_checkpoints/semi_automated_literature_search-checkpoint.py
@@ -157,8 +157,9 @@ def merge_search_results(headers):
     
     # process wos_poten_urls
     doi_df = pd.read_csv(FPM.wos_poten_urls, sep='delimiter')
-    doi_df = doi_df[['DOI']]
+    print(df.columns)
     print(doi_df.head())
+    doi_df = doi_df[['DOI']]
     doi_df.to_csv(FPM.path_poten_csv)
     
     # process pubmed_pmc_poten_urls
