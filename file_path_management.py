0635b7456760592fff8e0c2dc8d0ecac207b487c
Didi Hou <didi.hou@outlook.com>
2023-07-24 18:56:11 +0200

    /

--- file_path_management.py
+++ file_path_management.py
@@ -1,22 +1,24 @@
 # annoucing all file paths
-project_folder = '/Users/didihou/myProjects/didihou_master_project'
-litera_pdf_folder = '/Users/didihou/myProjects/litera_pdfs'
-datasets_folder = '/Users/didihou/myProjects/didihou_master_project/datasets'
+project_folder = "/Users/didihou/myProjects/didihou_master_project"
+litera_pdf_folder = "/Users/didihou/myProjects/litera_pdfs"
+datasets_folder = "/Users/didihou/myProjects/didihou_master_project/datasets"
 
 
-seed_paper_list = '/Users/didihou/myProjects/didihou_master_project/datasets/seed_literature_list.txt'
-cocomac_paper_list = '/Users/didihou/myProjects/didihou_master_project/datasets/cocomac_literature_list.txt'
+seed_paper_list = "/Users/didihou/myProjects/didihou_master_project/datasets/seed_literature_list.txt"
+cocomac_paper_list = "/Users/didihou/myProjects/didihou_master_project/datasets/cocomac_literature_list.txt"
 
 
-poten_litera_gs = '/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_google_scholar.txt'
-poten_litera_wos = '/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_webofscience.csv'
-poten_litera_pmc = '/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_pmc.csv'
-poten_litera_eupmc = '/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_europepmc.csv'
+poten_litera_gs = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_google_scholar.csv"
+poten_litera_wos = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_webofscience.csv"
+poten_litera_pmc = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_pmc.csv"
+poten_litera_eupmc = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_europepmc.csv"
 
 
-poten_litera_csv = '/Users/didihou/myProjects/didihou_master_project/datasets/potential_related_literature.csv'
+poten_litera_csv = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_related_literature.csv"
 
+related_litera_csv = "/Users/didihou/myProjects/didihou_master_project/datasets/related_literature.csv"
 
+summary_table = "/Users/didihou/myProjects/didihou_master_project/datasets/summary_table_of_related_studies.csv"
 
 
 
--- public_library.py
+++ public_library.py
@@ -1,4 +1,6 @@
 # self-written public library
+import csv
+import pandas as pd
 
 # setting headers and proxies
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
@@ -26,11 +28,23 @@ def ask_ChatGPT(context, queries):
     # code
     return answers
             
-'''
-# test code: ask_ChatGPT(context, queries)
-context = ['', '']
-queries = ['', '']
-answers = ask_ChatGPT(context, queries)
-for answer in answers:
-    print(answers, '\n')
-'''
+
+# # test code: ask_ChatGPT(context, queries)
+# context = ['', '']
+# queries = ['', '']
+# answers = ask_ChatGPT(context, queries)
+# for answer in answers:
+#     print(answers, '\n')
+
+def add_row_to_csv(csv_path, new_row, columns):
+    df_new_row = pd.DataFrame(new_row, columns = columns)
+    with open(csv_path, 'r') as csvfile:
+        csv_dict = [row for row in csv.DictReader(csvfile)]
+        if len(csv_dict) == 0:
+            df_new_row.to_csv(csv_path, index = False, header = True)
+        else:
+            df_new_row.to_csv(csv_path, mode = 'a', index = False, header = False)
+
+
+# test code: add_rows_to_csv(path_potential, info_json, columns)
+# add_rows_to_csv(path_potential, info_json, columns)
