ff79417c636672242b2a57d509a0b3d7a92d4bc2
Didi Hou <didi.hou@outlook.com>
2023-07-26 11:06:51 +0200

    /

--- file_path_management.py
+++ file_path_management.py
@@ -3,7 +3,7 @@ project_folder = "/Users/didihou/myProjects/didihou_master_project"
 litera_pdf_folder = "/Users/didihou/myProjects/litera_pdfs"
 datasets_folder = "/Users/didihou/myProjects/didihou_master_project/datasets"
 
-http_proxy = "/Users/didihou/myProjects/didihou_master_project/http.txt"
+proxy = "/Users/didihou/myProjects/didihou_master_project/proxy.txt"
 
 seed_paper_list = "/Users/didihou/myProjects/didihou_master_project/datasets/seed_literature_list.txt"
 cocomac_paper_list = "/Users/didihou/myProjects/didihou_master_project/datasets/cocomac_literature_list.txt"
@@ -15,10 +15,11 @@ poten_litera_pmc = "/Users/didihou/myProjects/didihou_master_project/datasets/po
 poten_litera_eupmc = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_europepmc.csv"
 
 
-poten_litera_csv = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_related_literature.csv"
-auto_filtered = "/Users/didihou/myProjects/didihou_master_project/datasets/auto_filtered.csv"
+poten_litera = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_related_literature.csv"
+filtered_poten_litra = "/Users/didihou/myProjects/didihou_master_project/datasets/filtered_potential_literature.csv"
+ranked_poten_litera = "/Users/didihou/myProjects/didihou_master_project/datasets/ranked_potential_literature.csv"
 
-related_litera_csv = "/Users/didihou/myProjects/didihou_master_project/datasets/related_literature.csv"
+related_litera = "/Users/didihou/myProjects/didihou_master_project/datasets/related_literature.csv"
 
 summary_table = "/Users/didihou/myProjects/didihou_master_project/datasets/summary_table_of_related_studies.csv"
 
