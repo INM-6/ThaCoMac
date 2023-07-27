97d92b403a1d2720adffe6394fd2a1a56837b4b1
Didi Hou <didi.hou@outlook.com>
2023-07-27 16:38:44 +0200

    /

--- public_library.py
+++ public_library.py
@@ -73,7 +73,7 @@ def add_row_to_csv(csv_path, new_row, columns):
         with open(csv_path, 'r') as csvfile:
             csv_dict = [row for row in csv.DictReader(csvfile)]
             if len(csv_dict) == 0:
-                df_new_row.to_csv(csv_path, index = False, header = True)
+                df_new_row.to_csv(csv_path, index = False, header = True, encoding='utf-8-sig', sep = ",")
             else:
                 df_new_row.to_csv(csv_path, mode = 'a', index = False, header = False, encoding='utf-8-sig', sep = ",")
     except:
