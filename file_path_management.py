791c8cf9c715b224ca8811498313ad1f6ee22dd6
Didi Hou <d.hou@outlook.com>
2023-08-03 11:19:27 +0200

    update 7 files and create 1 file

--- file_path_management.py
+++ file_path_management.py
@@ -1,39 +1,43 @@
+from pathlib import Path
+import os
+
+
 # annoucing all file paths
-project_folder = "/Users/didihou/myProjects/didihou_master_project"
-litera_pdf_folder = "/Users/didihou/myProjects/litera_pdfs"
-datasets_folder = "/Users/didihou/myProjects/didihou_master_project/datasets"
+project_folder = Path.cwd()
+litera_pdf_folder = os.path.join(project_folder, "litera_pdfs")
+datasets_folder = os.path.join(project_folder, "datasets")
 
 
-proxy_list = "/Users/didihou/myProjects/didihou_master_project/list_proxyseller.txt"
+proxy_list = os.path.join(project_folder, "list_proxyseller.txt")
 
 
-seed_paper_list = "/Users/didihou/myProjects/didihou_master_project/datasets/seed_literature_list.txt"
-cocomac_paper_list = "/Users/didihou/myProjects/didihou_master_project/datasets/cocomac_literature_list.txt"
+seed_paper_list = os.path.join(datasets_folder, "seed_literature_list.txt")
+cocomac_paper_list = os.path.join(datasets_folder, "cocomac_literature_list.txt")
 
 
-poten_litera_gs = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_google_scholar.csv"
-poten_litera_gs_test = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_google_scholar_test.csv"
-poten_litera_gs_processed_step1 = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_google_scholar_processed_step1.csv"
-poten_litera_gs_processed_step2 = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_google_scholar_processed_step2.csv"
-poten_litera_wos_1 = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_webofscience_1.csv"
-poten_litera_wos_2 = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_webofscience_2.csv"
-poten_litera_wos = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_webofscience.csv"
-poten_litera_wos_processed = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_webofscience_processed.csv"
-poten_litera_pubmed = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_pubmed.csv"
-poten_litera_pubmed_processed = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_pubmed_processed.csv"
-poten_litera_eupmc = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_europepmc.csv"
-poten_litera_eupmc_processed = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_literature_europepmc_processed.csv"
+poten_litera_gs = os.path.join(datasets_folder, "potential_literature_google_scholar.csv")
+poten_litera_gs_test = os.path.join(datasets_folder, "potential_literature_google_scholar_test.csv")
+poten_litera_gs_processed_step1 = os.path.join(datasets_folder, "potential_literature_google_scholar_processed_step1.csv")
+poten_litera_gs_processed_step2 = os.path.join(datasets_folder, "potential_literature_google_scholar_processed_step2.csv")
+poten_litera_wos_1 = os.path.join(datasets_folder, "potential_literature_webofscience_1.csv")
+poten_litera_wos_2 = os.path.join(datasets_folder, "potential_literature_webofscience_2.csv")
+poten_litera_wos = os.path.join(datasets_folder, "potential_literature_webofscience.csv")
+poten_litera_wos_processed = os.path.join(datasets_folder, "potential_literature_webofscience_processed.csv")
+poten_litera_pubmed = os.path.join(datasets_folder, "potential_literature_pubmed.csv")
+poten_litera_pubmed_processed = os.path.join(datasets_folder, "potential_literature_pubmed_processed.csv")
+poten_litera_eupmc = os.path.join(datasets_folder, "potential_literature_europepmc.csv")
+poten_litera_eupmc_processed = os.path.join(datasets_folder, "potential_literature_europepmc_processed.csv")
 
 
-poten_litera = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_related_literature.csv"
-filtered_poten_litra = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_related_literature_filtered.csv"
-ranked_poten_litera = "/Users/didihou/myProjects/didihou_master_project/datasets/potential_related_literature_ranked.csv"
+poten_litera = os.path.join(datasets_folder, "potential_related_literature.csv")
+filtered_poten_litra = os.path.join(datasets_folder, "potential_related_literature_filtered.csv")
+ranked_poten_litera = os.path.join(datasets_folder, "potential_related_literature_ranked.csv")
 
 
-related_litera = "/Users/didihou/myProjects/didihou_master_project/datasets/related_literature.csv"
+related_litera = os.path.join(datasets_folder, "related_literature.csv")
 
 
-summary_table = "/Users/didihou/myProjects/didihou_master_project/datasets/summary_table_of_related_studies.csv"
+summary_table = os.path.join(datasets_folder, "summary_table_of_related_studies.csv")
 
 
 
