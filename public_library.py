b7efff5909f3fe08b5665d1cdf180824f30a7387
Didi Hou <didi.hou@outlook.com>
2023-08-07 22:29:49 +0200

    update public_library.py and test_code.ipynb

--- public_library.py
+++ public_library.py
@@ -17,14 +17,15 @@ import random
 from requests.auth import HTTPProxyAuth
 from metapub import FindIt
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import TimeoutException, WebDriverException, NoSuchElementException
-import os
+import re
 
 
 # setting headers and proxies
