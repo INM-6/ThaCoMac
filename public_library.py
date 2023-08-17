679ec1a33c21f4129487bc3579735c7b16557611
Didi Hou <d.hou@outlook.com>
2023-08-17 14:15:19 +0200

    update public_library.py and test_code.ipynb

--- public_library.py
+++ public_library.py
@@ -29,7 +29,8 @@ import re
 
 
 # setting headers and proxies
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+# headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
 def get_proxies():
     with open(fpath.proxy_list) as f:
         proxy_list = f.readlines()
