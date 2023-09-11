8864f7cbebea13ad55bc9d46525b0be38e9d245e
Didi Hou <didi.hou@outlook.com>
2023-09-11 22:19:23 +0200

    update 4 files and create 1 file

--- file_path_management.py
+++ file_path_management.py
@@ -36,6 +36,7 @@ poten_litra_filtered = os.path.join(datasets_folder, "potential_related_literatu
 poten_litera_ids_ftl_filled = os.path.join(datasets_folder, "potential_related_literature_ids_ftl_filled.csv")
 poten_litera_ids_ftl_filled_filtered = os.path.join(datasets_folder, "potential_related_literature_ids_ftl_filled_filtered.csv")
 poten_litera_testing_set_300 = os.path.join(datasets_folder, "potential_related_literature_testing_set_300.csv")
+poten_litera_testing_set_300_read = os.path.join(datasets_folder, "potential_related_literature_testing_set_300_read.csv")
 poten_litera_litera_db = os.path.join(datasets_folder, "potential_related_literature_databse.csv")
 poten_litera_litera_db_ranked = os.path.join(datasets_folder, "potential_related_literature_database_ranked.csv")
 
