bb7b578f90db79fc34c3776d46487c1a7fba37bf
Didi Hou <d.hou@outlook.com>
2023-10-09 08:28:02 +0200

    update data_preprocessing.ipynb and public_library.py

--- public_library.py
+++ public_library.py
@@ -323,8 +323,8 @@ def pmcid2doi_pmid(pmcid):
 
 # process text
 def process_text(text, lower):
-    # keep only ascii characters
-    text = ''.join(char for char in text if ord(char) < 128)
+    # keep only non control characters of ascii characters
+    text = ''.join(ch for ch in text if 32 <= ord(ch) <= 126)
     # text = text.encode('ascii', 'ignore').decode('ascii') 
 
     # remove SOH (start of heading)
